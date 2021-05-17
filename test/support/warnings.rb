361bb6d49a9644ec0fc5652bc22cdd52316124b1
Matias Grunberg <matias@yellowspot.dev>
2021-05-17 11:42:18 -0300

    Remove Warning.ignore. http-parser is not a dependency anymore

--- test/support/warnings.rb
+++ test/support/warnings.rb
@@ -2,5 +2,4 @@ if RUBY_VERSION >= "2.4"
   require "warning"
 
   Warning.process('', /instance variable @\w+ not initialized/ => :raise)
-  Warning.ignore(/interpreted as argument prefix/, Gem::Specification.find_by_name("http-parser").load_paths.first)
 end
