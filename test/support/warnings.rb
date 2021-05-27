fb14ff446439fcde077c4ac6af202754d7ea09bd
Matias Grunberg <matias@yellowspot.dev>
2021-05-27 16:40:07 -0300

    Allow http 5-x (#55)
    * Allow Down::Http to be used with HTTP.rb 5.x
    
    * Remove Warning.ignore. http-parser is not a dependency anymore
    
    * Update readme: use http 5.0
    
    * Use http 4.x for ruby 2.4 and 2.3
    
    * add a comment

--- test/support/warnings.rb
+++ test/support/warnings.rb
@@ -2,5 +2,9 @@ if RUBY_VERSION >= "2.4"
   require "warning"
 
   Warning.process('', /instance variable @\w+ not initialized/ => :raise)
-  Warning.ignore(/interpreted as argument prefix/, Gem::Specification.find_by_name("http-parser").load_paths.first)
+
+  # ruby 2.5 or higher uses http.rb 5.0 which doesn't have http-parser as a dependency
+  if RUBY_VERSION < "2.5"
+    Warning.ignore(/interpreted as argument prefix/, Gem::Specification.find_by_name("http-parser").load_paths.first)
+  end
 end
