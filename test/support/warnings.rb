1a08069e0234d308f6ad71fcfc054a055aa4e57b
Janko Marohnić <janko.marohnic@gmail.com>
2023-10-04 10:47:59 +0200

    Ignore Ruby warnings from all gems

--- test/support/warnings.rb
+++ test/support/warnings.rb
@@ -1,12 +1,7 @@
 if RUBY_VERSION >= "2.4"
   require "warning"
 
-  Warning.process('', /instance variable @\w+ not initialized/ => :raise)
-
-  Warning.ignore(/instance variable @\w+ not initialized/, Gem::Specification.find_by_name("httpx").load_paths.first)
-
-  # ruby 2.5 or higher uses http.rb 5.0 which doesn't have http-parser as a dependency
-  if RUBY_VERSION < "2.5"
-    Warning.ignore(/interpreted as argument prefix/, Gem::Specification.find_by_name("http-parser").load_paths.first)
+  Gem.path.each do |path|
+    Warning.ignore(//, path)
   end
 end
