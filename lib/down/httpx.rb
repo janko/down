ed0cc80ad76db07859774d8cf30188aa6515c536
Mark Bumiller <mark.bumiller@wurl.com>
2026-02-19 08:20:07 -0500

    fix

--- lib/down/httpx.rb
+++ lib/down/httpx.rb
@@ -130,7 +130,7 @@ module Down
       end
       client = block.call(client) if block
 
-      client.request(method.to_s.upcase, uri,stream: true, **options)
+      client.request(method.to_s.upcase, uri, stream: true, **options)
     rescue => exception
       request_error!(exception)
     end
